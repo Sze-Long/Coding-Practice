0115f4a3ab8ebfde7561ae1956923f0d50f6d085
Sze-Long <lamszelong9@gmail.com>
2024-11-04 11:19:44 -0500

    Uploading old Codeforces problems

---
+++ Codeforces/1968A_Maximize.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+    int a;
+    int b;
+    cin >> a;
+ 
+    for (a; a>0; a--){
+        cin >> b;
+        cout << b - 1 << endl;
+ 
+        }
+ 
+ 
+    return 0;
+    
+}
